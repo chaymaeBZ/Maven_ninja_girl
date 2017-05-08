93f68520e072d87c68f13b64a2b6227785296fee
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2017-05-08 01:04:17 +0100

    spring conflicts solved

--- ninja_application.rb
+++ ninja_application.rb
@@ -10,10 +10,13 @@ if yes?("Would you like to install Devise?")
   gem 'Devise'
   run "bundle install"
 end
-after_bundle do 
+
+after_bundle do
+  run "spring stop" 
   generate "devise:install"
   model_name = ask("What would you like to name your user model? (the default is user)")
   model_name = "user" if model_name.blank?
   generate "devise", model_name
-  say "We're done with dirty job, now focus on great features - Ninja girl 👩‍💻 "
+  say "\033[1mWe're done with dirty job, now focus on great features - Ninja girl\033[0m  👩‍💻  "  say "byebye :)"
+
 end
