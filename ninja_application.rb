1ab19777d9422973161a01c5d06461552955c4a9
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2017-05-08 03:53:38 +0100

    some changes

--- ninja_application.rb
+++ ninja_application.rb
@@ -7,16 +7,20 @@ gem_group :test do
 end
 
 if yes?("Would you like to install Devise?")
-  gem 'Devise'
-  run "bundle install"
-end
-
-after_bundle do
-  run "spring stop" 
-  generate "devise:install"
-  model_name = ask("What would you like to name your user model? (the default is user)")
-  model_name = "user" if model_name.blank?
-  generate "devise", model_name
-  say "\033[1mWe're done with dirty job, now focus on great features - Ninja girl\033[0m  👩‍💻  "  say "byebye :)"
+  gem "devise"
 
 end
+
+#if yes?("do u want to bundle now")  
+#  run "bundle install"
+#end
+#after_bundle do
+#  run "spring stop"
+#  generate "devise:install"
+#  model_name = ask("What would you like to name your user model? (the default is user)")
+#  model_name = "user" if model_name.blank?
+#  generate "devise", model_name
+#  say "\033[1mWe're done with dirty job, now focus on great features - Ninja girl\033[0m  👩‍💻 "
+#  say "byebye :)"
+
+#end
