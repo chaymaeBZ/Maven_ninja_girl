b4410cd0892d6611da0e41edacc8443c35b334d8
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2017-05-08 20:26:30 +0100

    extra configurations for bootstrap

--- ninja_application.rb
+++ ninja_application.rb
@@ -1,4 +1,5 @@
 require_relative 'custom_behaviour'
+
 gem_group :test do
   gem "rspec-rails"
   gem "guard-rspec"
@@ -9,28 +10,42 @@ end
 
 if yes?("Would you like to install Devise?")
   gem "devise"
-
+  @using_devise? = true
 end
 
 if yes?("do you want to use Bootstrap for your frontend ?")
+
   gem 'bootstrap-sass'
   gem 'autoprefixer-rails'
+
+  # bootstrap extra configurations
   inject_into_file 'app/assets/javascripts/application.js', after: "//= require jquery\n" do
     "//= require bootstrap-sprockets\n"
   end 
+
+  inside ('app/assets/stylesheets') do
+    run 'mv application.css application.css.sass'
+    inject_into_file 'application.css.sass', after: "*/\n" do
+      '@import "bootstrap-sprockets"\n'
+      '@import "bootstrap"\n'
+    end
+  end
+
 end
 
 if yes?("do u want to bundle now")  
   run "bundle install"
 end
 after_bundle do
-  run "spring stop"
-  generate "devise:install"
-  model_name = ask("What would you like to name your user model? (the default is user)")
-  model_name = "user" if model_name.blank?
-  generate "devise", model_name
+  if @using_devise?
+    run "spring stop"
+    generate "devise:install"
+    model_name = ask("What would you like to name your user model? (the default is user)")
+    model_name = "user" if model_name.blank?
+    generate "devise", model_name
+  end
   rake "db:migrate"  
   say "\033[1mWe're done with dirty job, now focus on great features - Ninja girl\033[0m  👩‍💻 "
-  say " \033[1;35mbyebye :)\033[0m "
+  say " \033[1;35mbyebye :)\033[0m\n "
 
 end
