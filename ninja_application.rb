073d9b4483af456f0f032a58c219b67910eb707f
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2017-05-08 04:05:58 +0100

    some enhancements and typos ...

--- ninja_application.rb
+++ ninja_application.rb
@@ -11,16 +11,16 @@ if yes?("Would you like to install Devise?")
 
 end
 
-#if yes?("do u want to bundle now")  
-#  run "bundle install"
-#end
-#after_bundle do
-#  run "spring stop"
-#  generate "devise:install"
-#  model_name = ask("What would you like to name your user model? (the default is user)")
-#  model_name = "user" if model_name.blank?
-#  generate "devise", model_name
-#  say "\033[1mWe're done with dirty job, now focus on great features - Ninja girl\033[0m  👩‍💻 "
-#  say "byebye :)"
+if yes?("do u want to bundle now")  
+  run "bundle install"
+end
+after_bundle do
+  run "spring stop"
+  generate "devise:install"
+  model_name = ask("What would you like to name your user model? (the default is user)")
+  model_name = "user" if model_name.blank?
+  generate "devise", model_name
+  say "\033[1mWe're done with dirty job, now focus on great features - Ninja girl\033[0m  👩‍💻 "
+  say " \033[1;35mbyebye :)\033[0m "
 
-#end
+end
